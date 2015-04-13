dc3080c9ceaa2eeb12a86ac2675f9c9670f7b27f
Thomas Hardee <bluesunrise85@gmail.com>
2015-04-13 10:53:01 -0400

    Upload of main files

---
+++ footer.php
@@ -0,0 +1,7 @@
+<div id="footer">
+<p>This is a footer</p>
+
+
+</div>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Project</title>
+<link rel="stylesheet" type="text/css" href="style-main">
+</head>
+<body>
+<div id="main">
+<h2>Title and Stuff</h2>
+<p>This is text.</p>
+
+</div>
